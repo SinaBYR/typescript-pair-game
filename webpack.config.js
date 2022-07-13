028a9877c93f9dd6c678f0d3a447e8ea6e6ef115
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-07-13 13:05:59 +0430

    Install webpack dev server, and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.ts'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader', include: [path.resolve(__dirname, 'src')] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'TypeScript Pair Game'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    compress: true,
+    port: 8080,
+    devMiddleware: {
+      writeToDisk: filePath => !/hot-update/i.test(filePath)
+    }
+  }
+}
