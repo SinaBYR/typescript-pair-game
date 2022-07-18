79686c18eb311d258ebc297392b0b275848a6f21
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-07-18 17:57:46 +0430

    Finish selectCard function, add new classes in order to control paired (completed-pairs) cards

--- src/components/gameCard.ts
+++ src/components/gameCard.ts
@@ -1,26 +1,25 @@
 import './gameCard.css';
+import { selectCard, pause } from '../game';
 
 type Props = {
-  no: string;
+  tag: string;
   src: any;
 }
 
-export function gameCard({ no, src }: Props) {
+export function gameCard({ tag, src }: Props) {
   const card = document.createElement('div');
   const front = document.createElement('div');
   const back = document.createElement('div');
 
   card.classList.add('gameCard');
-  card.setAttribute('data-card-number', no);
+  card.setAttribute('data-card-tag', tag);
 
   front.classList.add('front');
-
   back.classList.add('back');
   back.style.backgroundImage = `url(${src})`;
 
-  card.onclick = e => {
-    card.classList.toggle('clicked');
-  }
+  // To read more about "pause" variable, please refer to game.ts file.
+  card.onclick = () => !pause && selectCard(card);
 
   card.appendChild(front);
   card.appendChild(back);
---
+++ src/game.ts
@@ -0,0 +1,45 @@
+// The selected card is stored in "element" variable.
+let storedCard: HTMLDivElement|null = null;
+
+// The purpose of this variable is to block user from selecting card when they've already selected two cards.
+// It's used in gameCard component to check if a card can be selected or not.
+// When user has selected two cards, it means the selecting phase MUST be paused. Otherwise, they're allowed to play.
+let pause = false;
+
+function selectCard(card: HTMLDivElement) {
+  if(!storedCard) {
+    card.classList.add('clicked');
+    return storedCard = card;
+  }
+
+  // When upper if clause condition is not met, it means user has already picked a card before, and this selection is their second guess.
+  // So, it means card selecting phase MUST be pause from now on.
+  pause = true;
+
+  const storedCardTag = storedCard.getAttribute('data-card-tag');
+  const cardTag = card.getAttribute('data-card-tag');
+
+  if(storedCardTag === cardTag) {
+    storedCard.classList.add('paired');
+    card.classList.add('paired');
+
+    storedCard = null;
+    // Second card guess was right, so the user is getting ready to start over.
+    // Meaning, card selecting phase which was stopped earlier, is about to start.
+    pause = false;
+    return;
+  }
+
+  card.classList.add('clicked');
+
+  const timeout = setTimeout(() => {
+    card.classList.remove('clicked');
+    storedCard?.classList.remove('clicked');
+    storedCard = null;
+    // Second card guess was wrong, so the user is getting ready to start over.
+    // Meaning, card selecting phase which was stopped earlier, is about to start.
+    pause = false;
+  }, 1000)
+}
+
+export { selectCard, pause }
