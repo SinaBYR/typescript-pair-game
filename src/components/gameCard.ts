11f67e1b47aca3bd4351f231e7dd1f5266671ce0
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-07-19 13:50:45 +0430

    Add wrapper div around front and back divs in gameCard, Add animation to gameCard class

--- src/components/gameCard.ts
+++ src/components/gameCard.ts
@@ -8,21 +8,25 @@ type Props = {
 
 export function gameCard({ tag, src }: Props) {
   const card = document.createElement('div');
+  const wrapper = document.createElement('div');
   const front = document.createElement('div');
   const back = document.createElement('div');
 
   card.classList.add('gameCard');
-  card.setAttribute('data-card-tag', tag);
-
+  wrapper.classList.add('wrapper');
   front.classList.add('front');
   back.classList.add('back');
+
   back.style.backgroundImage = `url(${src})`;
+
+  card.setAttribute('data-card-tag', tag);
 
   // To read more about "pause" variable, please refer to game.ts file.
   card.onclick = () => !pause && selectCard(card);
 
-  card.appendChild(front);
-  card.appendChild(back);
+  wrapper.appendChild(front);
+  wrapper.appendChild(back);
+  card.appendChild(wrapper);
 
   return card;
 }
